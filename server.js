f9ec1e6c60763e000f9a62846897fa9ace2b79a9
Caleb Tutty <caleb@carnivallabs.com>
2014-12-08 23:18:45 +1300

    Basic server script

---
+++ server.js
@@ -0,0 +1,16 @@
+var express = require('express'),
+    popitApi  = require('popit-api');
+
+var app = express();
+
+// Configure the PopIt API app
+var apiApp = popitApi({
+  databaseName: 'mp-contacts'
+});
+
+// Mount the PopIt API app at the appropriate path
+app.use('/api', apiApp);
+
+// Start to listen
+app.listen(3000);
+console.log("API Server listening at http://127.0.0.1:3000/api");
